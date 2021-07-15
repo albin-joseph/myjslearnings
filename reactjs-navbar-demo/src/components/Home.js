1880a431e10a43e8906d14246a0df407fd090d18
albin.j <albin.j@codelynks.com>
2021-07-15 13:55:06 +0530

    update navbar bootstrap

--- reactjs-navbar-demo/src/components/Home.js
+++ reactjs-navbar-demo/src/components/Home.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Form, FormControl, Button, Container, Card, CardGroup} from 'react-bootstrap';
 
 const Home = () => {
     return(
         <div>
-            <h1>Home</h1>
+            <Container>
+                <CardGroup>
+                    <Card border="primary" style={{ width: '18rem', margin: '5px' }}>
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card border="secondary" style={{ width: '18rem', margin: '5px' }}>
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </Container>
         </div>
     );
 };
