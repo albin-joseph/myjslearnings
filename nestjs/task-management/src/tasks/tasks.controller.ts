906a16d0ea5af421b593d38bbd6f5de1fb7afdc5
albin-joseph <albinjoseph007@gmail.com>
2024-05-04 15:42:17 +0530

    add GET, POST task api

--- nestjs/task-management/src/tasks/tasks.controller.ts
+++ nestjs/task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private taskService: TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body('title') title: string, @Body('description') description: string): Task {
+        return this.taskService.createTask(title, description);
+    }
+}
---
+++ nestjs/task-management/src/tasks/tasks.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import {Task, TaskStatus} from './task.model';
+import {v4 as uuid} from 'uuid';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    public getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask( title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+        this.tasks.push(task);
+        return task;
+    }
+}
