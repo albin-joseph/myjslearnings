44599551a91a6a4f5ed283260012f6b31a3fb844
albin-joseph <albinjoseph007@gmail.com>
2024-05-04 16:50:22 +0530

    add delete and patch methods

--- nestjs/task-management/src/tasks/tasks.controller.ts
+++ nestjs/task-management/src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -12,8 +12,23 @@ export class TasksController {
         return this.taskService.getAllTasks();
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(id)
+    }
+
     @Post()
-    createTask(createTaskDto: CreateTaskDto): Task {
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.taskService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+        return this.taskService.deleteTask(id)
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+        return this.taskService.updateTaskStatus(id,status)
     }
 }
--- nestjs/task-management/src/tasks/tasks.service.ts
+++ nestjs/task-management/src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
     createTask(createTaskDto: CreateTaskDto): Task {
         const {title, description} = createTaskDto;
         const task: Task = {
@@ -20,6 +24,16 @@ export class TasksService {
             status: TaskStatus.OPEN
         };
         this.tasks.push(task);
+        return task;
+    }
+
+    deleteTask(id: string): void {
+       this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = status;
         return task;
     }
 }
