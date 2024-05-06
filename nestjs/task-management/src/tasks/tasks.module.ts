89ba167bc0bb85625452f9309bf388718fbf483b
albin-joseph <albinjoseph007@gmail.com>
2024-05-06 08:57:10 +0530

    add task repository

--- nestjs/task-management/src/tasks/tasks.module.ts
+++ nestjs/task-management/src/tasks/tasks.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from  '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TaskRepository } from './task.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TaskRepository])
+  ],
   controllers: [TasksController],
   providers: [TasksService]
 })
