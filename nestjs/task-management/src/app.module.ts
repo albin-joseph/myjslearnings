7036c2f2ce19f8eb7365f4e2ef90a145aab08bd8
albin-joseph <albinjoseph007@gmail.com>
2024-05-06 07:34:35 +0530

    add typeorm

--- nestjs/task-management/src/app.module.ts
+++ nestjs/task-management/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [TasksModule]
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Code@123',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+
+  ]
 })
 export class AppModule {}
