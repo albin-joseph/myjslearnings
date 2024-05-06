23505a1917f3bdacc9eb0e33354dc30b18ca9423
albin-joseph <albinjoseph007@gmail.com>
2024-05-06 07:41:51 +0530

    add task entity

---
+++ nestjs/task-management/src/tasks/task.entity.ts
@@ -0,0 +1,16 @@
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm'
+import { TaskStatus } from './task.model';
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string
+
+    @Column()
+    status: TaskStatus;
+}
