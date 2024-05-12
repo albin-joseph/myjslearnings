2264c24047d55ec5c457bb4ae253bd3a6695ab0f
albin-joseph <albinjoseph007@gmail.com>
2024-05-12 15:29:38 +0530

    add config validation schema

--- nestjs/task-management/src/app.module.ts
+++ nestjs/task-management/src/app.module.ts
@@ -3,11 +3,13 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`.env.stage.${process.env.STAGE}`]
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema
     }),
     TasksModule,
     //we used sync for we need to get the all config information from config when we start the app
---
+++ nestjs/task-management/src/config.schema.ts
@@ -0,0 +1,11 @@
+import * as Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+    STAGE: Joi.string().required(),
+    DB_HOST: Joi.string().required(),
+    DB_PORT: Joi.number().default(5432).required(),
+    DB_USERNAME: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
+    DB_DATABASE: Joi.string().required(),
+    JWT_SECRET: Joi.string().required()
+})
